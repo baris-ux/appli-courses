f0b540be1481a2bfb48bfba626eaa1267e1a8f69
baris <baris.ock.etudes@gmail.com>
2025-08-30 15:16:44 +0200

    ajout des boutons pour la liste de course

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.Container}>
-      <Text style={styles.text}>bonjour</Text>
+      <Text style={styles.text}>Liste de courses</Text>
+      <TouchableOpacity style={styles.ajoutProduit}>
+        <Text>Ajouter un produit</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.produitFrequent}>
+        <Text>Produit fréquent</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   Container: {
-    flexDirection: 'row',
     alignItems: 'center',
     justifyContent : 'center',
     gap: 8,
-    backgroundColor: '#d8cd95ff',
+    backgroundColor: '#6C5543',
     flex : 1,
   },
   text: {
     color: '#fdfdff', 
     fontSize: 20,
+  },
+  ajoutProduit: {
+    position: 'absolute',
+    bottom: 30,
+    backgroundColor : "#FFFFEC",
+    padding : 15,
+    borderRadius : 10,
+  },
+  produitFrequent: {
+    position: 'absolute',
+    bottom: 100,
+    backgroundColor : "#FFFFEC",
+    padding : 15,
+    borderRadius : 10,
   },
 });
